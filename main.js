540f98e763b61a9314193e5e4fadaaa546f5953a
John-Michael <johnmcnagy@gmail.com>
2022-07-28 00:13:58 -0400

    Added global const and fucntion for comp's choice

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+const choices = ["rock","paper","scissors"]
 function game() {
   //play the game
   //play five rounds
   //console based
 }
-function playRound(playerSelection, computerSelection) {}
-const playerChoice = "rock";
+function playRound() {
+    const playerSelection =
+
+}
+function playerChoice () {
+// get input from the player
+}
+function computerChoice() {
+    // get input from the computer
+    return choices[Math.floor(Math.random()*choices.length)]
+
+}

739ee264543c849415a74d0df8fcf95d6bbe564e
John-Michael <johnmcnagy@gmail.com>
2022-07-28 00:46:05 -0400

    Added function for player choice and check input

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
-const choices = ["rock","paper","scissors"]
+const choices = ["rock", "paper", "scissors"];
+
 function game() {
+  playRound();
   //play the game
   //play five rounds
   //console based
 }
 function playRound() {
-    const playerSelection =
-
+  const playerSelection = playerChoice();
+  const computerSelection = computerChoice();
 }
-function playerChoice () {
-// get input from the player
+function playerChoice() {
+  // get input from the player
+  let input = prompt("Type Rock, Paper, or Siccors");
+  while (input == null) {
+    input = prompt("Type Rock, Paper, or Scissors");
+  }
+  input = input.toLowerCase();
+  let check = validateInput(input);
+  if (check == true) {
+    console.log(input);
+  }
+  //console.log(input);
 }
 function computerChoice() {
-    // get input from the computer
-    return choices[Math.floor(Math.random()*choices.length)]
-
+  // get input from the computer
+  return choices[Math.floor(Math.random() * choices.length)];
 }
+function validateInput(choice) {
+  if (choices.includes(choice)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+game();
